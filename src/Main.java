8c2fcfd5524bf2aa435ad160af603c863fbd0f70
Maria-Cb-Alt <carolzinhabalassoni@gmail.com>
2023-04-05 01:11:50 -0300

    Primeiro commit de Java

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Esse é o Screen Match"); //Usae "sout" para abreviaçã da entrada de dados
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int launchYear = 2022;
+        System.out.println("Ano de lançamento: " + launchYear);
+        boolean includedPlan= true;
+
+        double movieNote = 8.1;
+        System.out.println("A nota do filme é: " + movieNote);
+
+        double media = (9.8 + 6.3 + 8.0)/3;
+        System.out.println(media);
+
+        String sinopse;
+            sinopse = "Filme de aventura com galã dos anos 80";
+        System.out.println(sinopse);
+
+
+        }
+
+    }
